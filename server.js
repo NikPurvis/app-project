42d3e8db23204c9fd8094537633f6a633d20fdf4
NikPurvis <nikpurvis@gmail.com>
2022-03-15 19:53:15 -0600

    Added song position conditionals with associated icons

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const middleware = require('./utils/middleware')
 const SongsRouter = require('./controllers/songs')
 // const UserRouter = require('./controllers/user')
 // const User = require("./models/user")
-const Songs = require("./models/songs.js")
+
 // SEE MORE DEPENDENCIES IN ./utils/middleware.js
 // user and resource routes linked in ./utils/middleware.js
 
@@ -25,22 +25,11 @@ middleware(app)
 // app.use('/auth', UserRouter)
 app.use('/songs', SongsRouter)
 
+
 app.get('/', (req, res) => {
     const { username, userId, loggedIn } = req.session
 	res.render('index.liquid', { loggedIn, username, userId })
 })
-
-
-// INDEX route for all songs
-app.get("/songs/list", (req, res) => {
-    // res.send("/songs route")
-    Songs.find({})
-    .then(songs => {
-        res.render("songs", {songs: songs})
-    })
-    .catch(error => res.json(error))
-})
-
 
 
 app.get('/error', (req, res) => {
@@ -53,7 +42,6 @@ app.get('/error', (req, res) => {
 app.all('*', (req, res) => {
 	res.redirect('/error')
 })
-
 
 
 //////////////////////////////
